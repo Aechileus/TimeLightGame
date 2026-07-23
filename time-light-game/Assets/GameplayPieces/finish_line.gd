260558d7af41f865eb2bd4f35b39a25d269f7e50
Aechileus <dawsonmkrarup@gmail.com>
2026-07-23 10:30:38 -0700

    Time Stop Zone

--- time-light-game/Assets/GameplayPieces/finish_line.gd
+++ time-light-game/Assets/GameplayPieces/finish_line.gd
@@ -1,8 +1,10 @@
 extends Area3D
 
-func _physics_process(delta: float) -> void:
-	if self.has_overlapping_bodies():
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+# We need to check that its a player entering otherwise we just reload constantly if its 
+# anything else entering including its own taurus ring
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
-	
-
-
---
+++ time-light-game/Assets/GameplayPieces/time_stop_zone.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+@export var use_beeps: bool = true
+@export var one_shot: bool = false
+
+var _used: bool = false
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("player") or Global.is_time_stopped():
+		return
+	if one_shot and _used:
+		return
+	_used = true
+
+	if use_beeps:
+		Global.toggle_time_stop()
+	else:
+		Global.force_time_stop()
