c6fc28aeba088a7d66e491b834fa244c7719a782
Thesnake66six <seth_henry@icloud.com>
2026-07-26 01:27:54 +0100

    Added clock pickup

--- time-light-game/Assets/GameplayPieces/clock_pickup.gd
+++ time-light-game/Assets/GameplayPieces/clock_pickup.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+## The bonus time added when picking the clock up
+@export var bonus_time: float = 10.0
 @export_category("Animation")
 ## how far up and down it bobs from where it was placed
 @export var bob_height: float = 0.5
@@ -7,8 +9,14 @@ extends Node3D
 @export var bob_speed: float = 2.0
 ## how fast it spins around its y
 @export var spin_speed: float = 1.5
+## how fast the hands spin (rotations per second)
+@export var hand_spin_speed: float = 0.75
+
+
 
 @onready var pickup: Area3D = $Pickup
+@onready var minutes_hand: MeshInstance3D = $clock_2/clock_2_arm_minutes
+@onready var hours_hand: MeshInstance3D = $clock_2/clock_2_arm_hours
 
 var base_y: float = 0.0
 var time: float = 0.0
@@ -19,20 +27,16 @@ func _ready() -> void:
 	pickup.body_entered.connect(on_body_entered)
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	time += delta
 	# bob around the placed height, spin keeps ticking on top of it
-	position.y = base_y + sin(time * bob_speed) * bob_height
-	rotate_y(spin_speed * delta)
+	#position.y = _base_y + sin(_time * bob_speed) * bob_height
+	#rotate_y(spin_speed * delta)
+	minutes_hand.rotate_z(2 * PI * hand_spin_speed * delta)
+	hours_hand.rotate_z(-2 * PI * hand_spin_speed/3.0 * delta)
 
 
 func on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("player"):
 		return
-	var node := body as Node
-	while node:
-		if node.has_method("heal_to_full"):
-			node.heal_to_full()
-			queue_free()
-			return
-		node = node.get_parent()
+	SignalBus.change_level_time.emit(bonus_time)
